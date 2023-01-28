ec690fa750105a2fe92a8911164f7ef512f787bd
LAXMIDHAR DAS <laxmidhardas0000@gmail.com>
2023-01-28 11:02:57 +0530

    login registration

--- src/Components/LoginForm/Login.jsx
+++ src/Components/LoginForm/Login.jsx
@@ -1,26 +1,77 @@
 import React from 'react';
-import '../LoginForm/Login.css';
 
 const Login = () => {
-  const popup = () => {
-    alert('login-popup');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('You clicked submit.');
   };
-
   return (
-    <div className="cover">
-      <h1>Login</h1>
-      <select>
-        <option>Doctor</option>
-        <option>Patient</option>
-        <option>Admin</option>
-        <option>Receptionist</option>
-      </select>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
-      <div className="login-btn" onClick={popup}>
-        LOGIN
+    <section class="vh-100 gradient-custom">
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div
+              class="card bg-success text-white"
+              style={{ borderRadius: '1rem' }}
+            >
+              <div class="card-body p-5 text-left">
+                <div class="mb-md-5 mt-md-4 pb-5">
+                  <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                  <p class="text-white-50 mb-5">
+                    Please enter your login and password!
+                  </p>
+
+                  <div class="form-outline form-white mb-4">
+                    <label class="form-label" for="typeEmailX">
+                      Username
+                    </label>
+                    <input
+                      type="email"
+                      id="typeEmailX"
+                      class="form-control form-control-lg"
+                    />
+                  </div>
+
+                  <div class="form-outline form-white mb-4">
+                    <label class="form-label" for="typePasswordX">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      id="typePasswordX"
+                      class="form-control form-control-lg"
+                    />
+                  </div>
+
+                  <p class="small mb-5 pb-lg-2">
+                    <a class="text-white-50" href="#!">
+                      Forgot password?
+                    </a>
+                  </p>
+
+                  <button
+                    class="btn btn-outline-light btn-lg px-5"
+                    type="submit"
+                    onSubmit={handleSubmit}
+                  >
+                    Login
+                  </button>
+                </div>
+
+                <div>
+                  <p class="mb-0">
+                    Don't have an account?{' '}
+                    <a href="#!" class="text-white-50 fw-bold">
+                      Sign Up
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
